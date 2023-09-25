96a0b0fbf0308981dfedd4caf18552ce5e63acbc
Omarr-kh <omar1752003@gmail.com>
2023-09-25 05:51:05 -0700

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,15 +13,15 @@ void _swap(listint_t **head, listint_t **node1, listint_t *node2)
 	{
 		node2->next->prev = *node1;
 	}
-	node2->next = *node1;
 	node2->prev = (*node1)->prev;
+	node2->next = *node1;
 	if ((*node1)->prev != NULL)
 	{
 		(*node1)->prev->next = node2;
 	}
 	else
 	{
-		(*head) = node2;
+		*head = node2;
 	}
 	(*node1)->prev = node2;
 	*node1 = node2->prev;
@@ -34,21 +34,19 @@ void _swap(listint_t **head, listint_t **node1, listint_t *node2)
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *i, *prev_node, *next_node;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (i = (*list)->next; i != NULL; i = next_node)
 	{
-		listint_t *i, *prev_node, *next_node;
-
-		if (list == NULL || *list == NULL || (*list)->next == NULL)
-			return;
-
-		for (i = (*list)->next; i != NULL; i = next_node)
+		next_node = i->next;
+		prev_node = i->prev;
+		while (prev_node != NULL && i->n < prev_node->n)
 		{
-			next_node = i->next;
-			prev_node = i->prev;
-			while (prev_node != NULL && i->n < prev_node->n)
-			{
-				_swap(list, &prev_node, i);
-				print_list((const listint_t *)*list);
-			}
+			_swap(list, &prev_node, i);
+			print_list((const listint_t *)*list);
 		}
 	}
 }
